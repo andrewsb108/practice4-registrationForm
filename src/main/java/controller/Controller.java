a683288ff7e02561e422715fc8d74c5fc0b9a1d1
andrewsb108 <andrey.barsuk1@gmail.com>
2021-07-06 08:38:09 +0300

    Was added main mvc classes

---
+++ src/main/java/controller/Controller.java
@@ -0,0 +1,18 @@
+package controller;
+
+import model.Model;
+import view.View;
+
+public class Controller {
+    private Model model;
+    private View view;
+
+    public Controller(Model model, View view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void processUser() {
+
+    }
+}
