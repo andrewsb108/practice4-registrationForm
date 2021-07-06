c9673a87dc0cd7556f2bd65781d0ba029c687c73
andrewsb108 <andrey.barsuk1@gmail.com>
2021-07-06 09:42:06 +0300

    Was added Main and View real-ion

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
 import controller.Controller;
 import model.Model;
 import view.View;
-
+/**
+ * Basis class of the program for launching application
+ * @author Andrii Barsuk
+ */
 public class Main {
     public static void main(String[] args) {
     Controller controller = new Controller(new Model(), new View());
--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -2,7 +2,14 @@ package controller;
 
 import model.Model;
 import view.View;
-
+/**
+ * class Controller gets data from console line and validates it.
+ * Controller uses Model for business logic
+ * and View in order to output validation info
+ * @author Andrii Barsuk
+ * @see Model
+ * @see View
+ */
 public class Controller {
     private Model model;
     private View view;
